d8bbec4abb9290382b23132bd13be571b915fd13
daveaugerinos <dave.augerinos@gmail.com>
2017-03-02 09:01:52 -0800

    Initial setup

---
+++ Receipts/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Receipts
+//
+//  Created by Dave Augerinos on 2017-03-02.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Receipts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Receipts
+//
+//  Created by Dave Augerinos on 2017-03-02.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
